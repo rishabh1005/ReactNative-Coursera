9540e64fcbc8a249cc2d633465eed1f216fb9290
Rishabh Khandelwal <rishabh.9659@gmail.com>
2020-07-08 19:47:20 +0530

    Navigation Part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,30 +1,37 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
-import { View } from 'react-native';
-import { DISHES } from '../shared/dishes';
+import { View , Platform} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Stack = createStackNavigator();
+
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
-        };
-    }
-
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId})
-    }
-
-
+        
     render() {
-
         return (
-            <View>
-                <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-            </View>
+            
+                <NavigationContainer>
+                    <Stack.Navigator
+                    initialRouteName = {"Menu"}
+                    screenOptions =  {{
+                        
+                        headerStyle: {
+                            backgroundColor: "#512DA8"
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            color: "#fff"            
+                        }
+                    }}>
+                        <Stack.Screen name="Menu" component={Menu}/>
+                        <Stack.Screen name="Dishdetail" options={{title:'Dish Detail'}} component={Dishdetail}/>
+                    </Stack.Navigator>
+                </NavigationContainer>
+            
         );
     }
 }
